5ffe5b35ccf3f55bac23a2526e48fbbb2281fbb8
Siddika-akhter <siddika.tas28@gmail.com>
2020-08-07 16:55:49 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,20 @@
+// falsy value:
+// 0
+// ""
+// undefined
+// null
+// NaN
+// false
+// truthy value:
+// '0', ' ', [], {}
+
+// const age = 4;
+// const name = '0'
+// let name;
+console.log(name);
+if(name){
+    console.log('condition is true')
+}
+else{
+    console.log('condition is false')
+}
