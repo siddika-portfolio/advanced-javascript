fa258e6bc34f5a357889efb1b7cca62158a438a5
Siddika-akhter <siddika.tas28@gmail.com>
2020-08-09 01:42:51 +0600

    object complete

---
+++ object-property.js
@@ -0,0 +1,15 @@
+const students = [
+    {id: 21, name: 'Omar Sunny'},
+    {id: 31, name: 'Manna'},
+    {id: 41, name: 'Moyouri'},
+    {id: 71, name: 'deephjol'}
+];
+
+const names = students.map( s => s.name);
+const ids = students.map(s => s.id);
+const bigger = students .filter( s => s.id>40);
+const biggerOne = students .find( s => s.id>40)
+console.log(names);
+console.log(ids);
+console.log(bigger);
+console.log('ls biggerOne', biggerOne);
